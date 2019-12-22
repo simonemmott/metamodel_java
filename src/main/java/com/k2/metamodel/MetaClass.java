3a898849a7d98a192b51c40f6b0283f3f3f9d489
simon <simon.emmott@yahoo.co.uk>
2019-12-22 23:55:42 +0000

    Added MetaClass::new & MetaClass::instance
    All tests passed

--- src/main/java/com/k2/metamodel/MetaClass.java
+++ src/main/java/com/k2/metamodel/MetaClass.java
@@ -1,21 +1,58 @@
 package com.k2.metamodel;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.k2.metamodel.exception.MetaClassAlreadyExists;
+import com.k2.metamodel.exception.MetaClassDoesNotExist;
+
 public class MetaClass {
 	
 	final MetaModel metaModel;
 	final Class<?> cls;
+	final Map<String,DomainModel> domainModels;
+	MetaPackage metaPackage;
+	MetaClass superMetaClass;
+	final Set<MetaClass> subMetaClasses;
+	final Set<MetaField> declaredMetaFields;
+	final Set<MetaMethod> declaredMetaMethods;
+	boolean scanned = false;
 	
-	private MetaClass(MetaModel metaModel, Class<?> cls) {
+	
+	
+	MetaClass(MetaModel metaModel, Class<?> cls) {
 		this.metaModel = metaModel;
 		this.cls = cls;
+		this.domainModels = new HashMap<>();
+		this.subMetaClasses = new HashSet<>();
+		this.declaredMetaFields = new HashSet<>();
+		this.declaredMetaMethods = new HashSet<>();
 	}
 	
 	public static MetaClass instance(MetaModel metaModel, Class<?> cls) {
-		return new MetaClass(metaModel, cls);
+		MetaClass metaClass;
+		try {
+			metaClass = metaModel.metaClass(cls);
+		} catch (MetaClassDoesNotExist err) {
+			metaClass = new MetaClass(metaModel, cls);
+			try {
+				metaModel.add(metaClass);
+			} catch (MetaClassAlreadyExists e) {
+				throw new RuntimeException("THIS SHOULD NOT HAPPEN", e);
+			}
+		}
+		
+		return metaClass;
 	}
 	
 	public String canonicalClassName() {
 		return cls.getCanonicalName();
+	}
+
+	public Class<?> getModelledClass() {
+		return cls;
 	}
 
 }
--- src/test/java/com/k2/metamodel/MetaClassTest.java
+++ src/test/java/com/k2/metamodel/MetaClassTest.java
@@ -3,11 +3,19 @@ package com.k2.metamodel;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import com.k2.metamodel.exception.MetaClassDoesNotExist;
+import com.k2.metamodel.exception.MetaModelException;
+import com.k2.test.utils.PrivateFieldAccessor;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MetaClassTest {
@@ -15,9 +23,69 @@ public class MetaClassTest {
 	@Rule
 	public ExpectedException exceptionRule = ExpectedException.none();
 	
+	private static PrivateFieldAccessor<MetaClass,MetaModel> metaModelField = new PrivateFieldAccessor<>(MetaClass.class, "metaModel", MetaModel.class);
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static PrivateFieldAccessor<MetaClass,Class<?>> clsField = new PrivateFieldAccessor(MetaClass.class, "cls", Class.class);
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static PrivateFieldAccessor<MetaClass,Map<String,DomainModel>> domainModelsField = new PrivateFieldAccessor(MetaClass.class, "domainModels", Map.class);
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static PrivateFieldAccessor<MetaClass,Set<MetaClass>> subMetaClassesField = new PrivateFieldAccessor(MetaClass.class, "subMetaClasses", Set.class);
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static PrivateFieldAccessor<MetaClass,Set<MetaField>> declaredMetaFieldsField = new PrivateFieldAccessor(MetaClass.class, "declaredMetaFields", Set.class);
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static PrivateFieldAccessor<MetaClass,Set<MetaMethod>> declaredMetaMethodsField = new PrivateFieldAccessor(MetaClass.class, "declaredMetaMethods", Set.class);
+	private static PrivateFieldAccessor<MetaClass,Boolean> scannedField = new PrivateFieldAccessor<>(MetaClass.class, "scanned", Boolean.class);
+	private static PrivateFieldAccessor<MetaClass,MetaPackage> metaPackageField = new PrivateFieldAccessor<>(MetaClass.class, "metaPackage", MetaPackage.class);
+	private static PrivateFieldAccessor<MetaClass,MetaClass> superMetaClassField = new PrivateFieldAccessor<>(MetaClass.class, "superMetaClass", MetaClass.class);
+
+
 	@Test
-	public void test() {
+	public void test_new_instance() {
 		
+		MetaModel metaModel = mock(MetaModel.class);
+		
+		MetaClass metaClass = new MetaClass(metaModel, Integer.class);
+		
+		assertEquals(metaModel, metaModelField.get(metaClass));
+		assertEquals(Integer.class, clsField.get(metaClass));
+		assertNotNull(domainModelsField.get(metaClass));
+		assertNotNull(subMetaClassesField.get(metaClass));
+		assertNotNull(declaredMetaFieldsField.get(metaClass));
+		assertNotNull(declaredMetaMethodsField.get(metaClass));
+		assertFalse(scannedField.get(metaClass));
+		assertNull(metaPackageField.get(metaClass));
+		assertNull(superMetaClassField.get(metaClass));
+		
+	}
+	
+	@Test
+	public void test_instance_returns_existing_instance() throws MetaModelException {
+		
+		MetaModel metaModel = mock(MetaModel.class);
+		MetaClass integerMetaClass = mock(MetaClass.class);
+		when(metaModel.metaClass(Integer.class)).thenReturn(integerMetaClass);
+		
+		assertEquals(integerMetaClass, MetaClass.instance(metaModel, Integer.class));
+		verify(metaModel, times(1)).metaClass(Integer.class);
+		
+	}
+	
+	@Test
+	public void test_instance_returns_new_instance() throws MetaModelException {
+
+		MetaModel metaModel = mock(MetaModel.class);
+		when(metaModel.metaClass(Integer.class)).thenThrow(MetaClassDoesNotExist.class);
+
+		MetaClass metaClass = MetaClass.instance(metaModel, Integer.class);
+
+		ArgumentCaptor<MetaClass> metaClassCaptor = ArgumentCaptor.forClass(MetaClass.class);
+		verify(metaModel, times(1)).add(metaClassCaptor.capture());
+		assertEquals(metaClass, metaClassCaptor.getValue());
+		
+		assertEquals(Integer.class, metaClass.getModelledClass());
+		
+		
+
 	}
 
 }
